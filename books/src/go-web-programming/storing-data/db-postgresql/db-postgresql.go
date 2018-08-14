dbb1d7df69835fc23a2ebb08fc4e6624c36af891
codelieche <codelieche@gmail.com>
2018-08-14 11:32:43 +0800

    golang postgresql delete and update

--- books/src/go-web-programming/storing-data/db-postgresql/db-postgresql.go
+++ books/src/go-web-programming/storing-data/db-postgresql/db-postgresql.go
@@ -52,13 +52,75 @@ func (p *Post) Create() (err error) {
 	}
 }
 
-func main() {
-	//createTable()
-	log.Println("Start...")
+func (p *Post) Delete() error {
+	// 删除Post
+	query := "DELETE FROM posts where id = $1"
+	if result, err := Db.Exec(query, p.Id); err != nil {
+		log.Println("删除Post出错:", err.Error())
+		return err
+	} else {
+		log.Println(result)
+		return nil
+	}
+}
+
+func (p *Post) Update() (err error) {
+	// 更新Post
+	query := "UPDATE posts SET title=$2, author=$3, content=$4 WHERE id = $1"
+	if result, err := Db.Exec(query, p.Id, p.Title, p.Author, p.Content); err != nil {
+		panic(err)
+	} else {
+		log.Println(result)
+		return nil
+	}
+}
+
+func getPost(id int) (post Post, err error) {
+	post = Post{}
+	query := "SELECT id, title, author, content FROM posts WHERE id = $1"
+	err = Db.QueryRow(query, id).Scan(&post.Id, &post.Title, &post.Author, &post.Content)
+	return
+}
+
+func runCreate() {
 	// 创建帖子
 	p := Post{Title: "Hello Golang", Author: "codelieche", Content: "## Golang \n > This is a good!"}
 	log.Println(p)
 	p.Create()
 	log.Println(p)
+}
 
+func runDelete(id int) {
+	// 删除帖子
+	// 1. 先获取到帖子
+	if p, err := getPost(id); err != nil {
+		panic(err)
+	} else {
+		log.Println(p)
+		p.Delete()
+	}
+}
+
+func runUpdate(id int) {
+	// 更新帖子
+	// 1. 先获取到帖子
+	if p, err := getPost(id); err != nil {
+		panic(err)
+	} else {
+		log.Println(p)
+		p.Author = "update"
+		p.Content = "Update Test"
+		p.Update()
+	}
+}
+
+func main() {
+	//createTable()
+	log.Println("Start...")
+
+	//runCreate()
+
+	//runDelete(10)
+
+	runUpdate(9)
 }
