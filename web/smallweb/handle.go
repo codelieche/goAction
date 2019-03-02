832444f5debbef5dd78beda441af77c5ba199802
codelieche <codelieche@gmail.com>
2019-03-02 23:43:35 +0800

    change smallWeb IndexPage

--- web/smallweb/handle.go
+++ web/smallweb/handle.go
@@ -2,11 +2,57 @@ package smallweb
 
 import (
 	"encoding/json"
+	"log"
+	"net"
 	"net/http"
+	"os"
+	"time"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Index"))
+	var ips = []string{}
+	var hostName string
+	var err error
+
+	if hostName, err = os.Hostname(); err != nil {
+		http.Error(w, "获取主机名出错", 500)
+		return
+	}
+
+	if addrs, err := net.InterfaceAddrs(); err != nil {
+		log.Println(err.Error())
+		http.Error(w, "获取网卡的地址出错", 500)
+		return
+	} else {
+		//log.Println(addrs)
+		//log.Println(len(addrs))
+
+		for _, address := range addrs {
+			//log.Println(address)
+			if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					ip := ipnet.IP.String()
+					//log.Println(ip)
+					ips = append(ips, ip)
+				}
+			}
+		}
+
+		// 返回结果
+		response := IndexResponse{
+			HostName: hostName,
+			IP:       ips,
+			Datetime: time.Now(),
+		}
+		if data, err := json.Marshal(response); err != nil {
+
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(data)
+		}
+	}
+
+	//w.Write([]byte("Hello Index"))
 }
 
 func api(w http.ResponseWriter, r *http.Request) {
--- web/smallweb/types.go
+++ web/smallweb/types.go
@@ -1,8 +1,16 @@
 package smallweb
 
+import "time"
+
 type Config struct {
 	Host string // 监听的地址：默认127.0.0.1
 	Port int    // 监听的端口：默认8080
+}
+
+type IndexResponse struct {
+	HostName string    // 主机名
+	IP       []string  // Ip地址列表
+	Datetime time.Time // 访问时间
 }
 
 type ApiResponse struct {
